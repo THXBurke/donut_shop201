bcab19ce7071b16fcb83b573f569d07635c0632f
THXBurke <rachelwheels@gmail.com>
2015-11-22 11:23:51 -0800

    methods work

--- donut_shop.js
+++ donut_shop.js
@@ -1,25 +1,37 @@
-var DonutShop = function(numHoursOpen, minCustPerHour, maxCustPerHour, avgDonutsPerCust, name) {
-        this.numHoursOpen = numHoursOpen;
+var DonutShop = function(minCustPerHour, maxCustPerHour, avgDonutsPerCust, name) {
         this.minCustPerHour = minCustPerHour;
         this.maxCustPerHour = maxCustPerHour;
         this.avgDonutsPerCust = avgDonutsPerCust;
         this.name = name;
+        this.hourlyDonutArray = [];
+        this.totalDonuts = 0;
 
-		this.randomCustPerHour = Math.floor(Math.random() * (maxCustPerHour - minCustPerHour + 1)) + minCustPerHour;
+		this.randomCustPerHour = function(){
+		 return Math.floor(Math.random() * (maxCustPerHour - minCustPerHour + 1)) + minCustPerHour;
+		};
+
         this.getDonutsPerHour = function(){
-            return avgDonutsPerCust * this.randomCustPerHour;
+        	for (var i = 0; i < 11; i++){
+            this.hourlyDonutArray.push(Math.floor(avgDonutsPerCust * this.randomCustPerHour()));}
+    		return this.hourlyDonutArray;
         };
+
         this.getDonutsPerDay = function() {
-          return this.getDonutsPerHour() * numHoursOpen; 
+          for (var i = 0; i< this.hourlyDonutArray.length; i++){
+          	this.totalDonuts += this.hourlyDonutArray[i];
+          }; 
+          return this.totalDonuts;
         };
-          this.report = function(){
+
+        this.report = function(){
             console.log(name + " Requires " + this.getDonutsPerHour() + " Donuts Per Hour, and " + this.getDonutsPerDay() + " Donuts Per Day " );
           };
-           this.report(); 
+        
+        this.report(); 
    };
    
-    var capitolHill = new DonutShop(8, 4, 37, 2, "Capitol Hill"); 
-    var downtown = new DonutShop(8, 8, 43, 4.5, "Downtown");
-    var southLakeUnion = new DonutShop(8, 9, 23, 6.33, "South Lake Union");
-    var wedgwood = new DonutShop(8, 2, 28, 1.25, "Wedgwood");
-    var ballard = new DonutShop(8, 8, 58, 3.75, "Ballard"); 
+    var capitolHill = new DonutShop(4, 37, 2, "Capitol Hill"); 
+    var downtown = new DonutShop(8, 43, 4.5, "Downtown");
+    var southLakeUnion = new DonutShop(9, 23, 6.33, "South Lake Union");
+    var wedgwood = new DonutShop(2, 28, 1.25, "Wedgwood");
+    var ballard = new DonutShop(8, 58, 3.75, "Ballard"); 
