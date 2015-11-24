510d0602408da54391090f4d41c41191b3b5e003
THXBurke <rachelwheels@gmail.com>
2015-11-23 17:11:37 -0800

    add styling to html and css

--- donut_shop.js
+++ donut_shop.js
@@ -4,6 +4,9 @@
 	this.avgDonutsPerCust = avgDonutsPerCust;
 	this.franchise = franchise;
 	this.hourlyDonutArray = [];
+
+	var  hourlyDonutsArray= ["Location", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "Total"];
+	
 	this.totalDonuts = 0;
 
 	this.randomCustPerHour = function(){
