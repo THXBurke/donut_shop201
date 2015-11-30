44a9d9089dde0c3cb933a58b3e4c8f3d52dd4f5d
THXBurke <rachelwheels@gmail.com>
2015-11-29 18:30:10 -0800

    easter egg

--- donut_shop.js
+++ donut_shop.js
@@ -73,9 +73,11 @@ console.log("newShop", shop)
 document.getElementById("submit").addEventListener("click", newLocation);
 }; 
 
-
-
-
+var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function() {
+  jQuery('#egggif').fadeIn(500, function() {
+    window.setTimeout(function() { jQuery('#egggif').hide(); }, 5000);
+  });
+}).listen();
 
 
 
